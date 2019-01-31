7be390aa5d726d398a838596a1f65b2d6a9f711c
Aaron Trovillion <aaron.trovillion@gmail.com>
2019-01-31 15:46:21 -0500

    add route control view checkpoint work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -7,34 +7,15 @@ describe("routes : static", () => {
 
     describe("GET /", () => {
         
-        it("should return status code 200", (done) => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
+                expect(body).toContain("Welcome to Bloccit");
 
-                done();
+                //done();
             })
         })
     })
 
-    describe("GET /marco", () => {
-
-        it("should return status code 200", (done) => {
-
-            request.get(marco, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-
-                done();
-            })
-        })
-
-        it("should return the string 'polo'", (done) => {
-            
-            request.get(marco, (err, res, body) => {
-                expect(res.body).toBe('polo');
-
-                done();
-            })
-        })
-    })
 })
