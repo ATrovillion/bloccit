856629f14a1b39b1bc973b7c0683114b853e33ac
Aaron Trovillion <aaron.trovillion@gmail.com>
2019-02-09 17:29:33 -0500

    undid last commit by changing Topics back to Topic in banner.js model

--- src/db/models/banner.js
+++ src/db/models/banner.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Banner.associate = function(models) {
     // associations can be defined here
-    Banner.belongsTo(models.Topics, {
+    Banner.belongsTo(models.Topic, {
       foreignKey: "topicId",
       onDelete: "CASCADE",
     });
